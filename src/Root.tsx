92249cec901a517437cf1272a56fc5da484e2f86
Ldoppea <yannick.chiron@gmail.com>
2024-01-15 23:19:04 +0100

    feat: Make total video duration dynamic and set HD size

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,17 +2,31 @@ import React from 'react'
 import {Composition} from 'remotion'
 
 import {MyComposition} from './Composition'
+import { useVideos } from './components/Videos'
+import { useJingle } from './components/HumanTalkJingle'
+import { INTRO_DURATION_IN_FRAMES } from './components/intro/Intro'
 
 export const RemotionRoot: React.FC = () => {
+	const videos = useVideos()
+	const jingle = useJingle()
+
+	if (!videos || !jingle) {
+		return null
+	}
+	
+	const totalDuration = Math.round(
+		jingle.durationInFrames * 2 + INTRO_DURATION_IN_FRAMES + videos.maxVideoDurationInFrames
+	)
+
 	return (
 		<>
 			<Composition
 				id="HumanTalksAssembleVideo"
 				component={MyComposition}
-				durationInFrames={60}
+				durationInFrames={totalDuration}
 				fps={30}
-				width={1280}
-				height={720}
+				width={1920}
+				height={1080}
 			/>
 		</>
 	)
